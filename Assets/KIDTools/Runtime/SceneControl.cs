533b1195c7a7c00815f9527212bccb8b762924b9
KID <50666207+KID421@users.noreply.github.com>
2023-08-14 22:03:26 +0800

    Initial commit

---
+++ Assets/KIDTools/Runtime/SceneControl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace KID.Tools
+{
+    /// <summary>
+    /// 場景控制器
+    /// </summary>
+    public class SceneControl : MonoBehaviour
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Unity_Git_Demo_20230814
+ Unity Git 示範專案 2023.08.14
